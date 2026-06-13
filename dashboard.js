e0fc8b35202478006bc23731536e90bdaf6f6caf
fayruzy4 <najibfairuz56@gmail.com>
2026-06-13 20:43:09 +0700

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,2 +1,6 @@
 document.getElementById("weather-updated").textContent =
 "Tes berhasil";
+document.getElementById("weather-updated").textContent =
+"Tes berhasil";
+<script src="dashboard.js"></script>
+</body>
